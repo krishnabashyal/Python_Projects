14788f458801771eb4d05aa468f0b2a297697aed
krishnabashyal <krishna.hello23@gmail.com>
2023-07-18 21:57:32 +0545

    todo basics

---
+++ main.py
@@ -0,0 +1,4 @@
+yoo = "nothing just a variable"
+print('Tell me what you wanna do today ')
+task1 = input('enter your first task: ') #Ct+Sh+L to select and change same words at multiple places
+print("1.", task1,len(task1))
