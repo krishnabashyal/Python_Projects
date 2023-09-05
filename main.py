e33ad55b1d165099c0ade3671afe04a95a2250cd
krishnabashyal <krishna.hello23@gmail.com>
2023-09-05 21:00:19 +0545

    nothing much, just changing the output and using with

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ while True:
     match what_u_want:
         case "add":
             task= input("enter a task : ") +"\n"
-            file = open("todos.txt","r")
-            tasks= file.readlines()
-            file.close()
+        
+            with open("todos.txt","r") as file:
+                tasks=file.readlines() #no need to close file
+                
             tasks.append(task)
             
-            file= open("todos.txt","w")
-            file.writelines(tasks)
-            file.close()
+            with open("todos.txt","w") as file:
+                file.writelines(tasks)
+            
             
         case "show"|"gimme":  
             
             print ("o/ your tasks for today are:")  
-            file = open("todos.txt", 'r')
-            tasks = file.readlines()
-            file.close()
+            with open("todos.txt", 'r') as file:
+                tasks = file.readlines()
             
-            for index,items in enumerate(tasks):
+            #list comphrehension 
+            new_tasks= [items.strip("\n") for items in tasks] #this makes new list removing \n from items of list 
+            
+            for index,items in enumerate(new_tasks):
+                #items= items.strip("\n").. can also use this to remove \n 
                 items = items.title()
                 index=index+1
                 sn=f"{index}.{items}" #fstring to eliminate :D extra spaces 
@@ -30,34 +34,29 @@ while True:
                 print(sn)
                        
         case 'edit':
-            # file = open("todos.txt","r")
-            # tasks= file.readlines()
-            # file.close()
             
+            with open("todos.txt","r") as file:
+                tasks= file.readlines()
+           
             num = int(input("which number to edit?: "))
             num= num-1
-            # given_task = tasks[num-1]
+           
             new_task =input("enter the new task: ") +"\n"
             tasks[num]=new_task
-            file = open("todos.txt","w")
-            tasks=file.writelines()
-            file.close()
             
-            # print("the new list is ")
-         
+            with open("todos.txt","w") as file:
+                tasks=file.writelines(tasks)
+            
         case "complete"|"completed"|"finished"|"remove":
-            file = open("todos.txt","r")
-            tasks=file.readlines()
-            file.close()
+            with open("todos.txt","r") as file:
+                tasks=file.readlines()
             
             comp_num= int(input("which numebr task did you complete?: "))
             comp_task= tasks.pop(comp_num-1).strip()
+            with open("todos.txt","w") as file:
             
+                file.writelines(tasks)
             
-            file= open("todos.txt","w")
-            file.writelines(tasks)
-            file.close()
-           
             print("successfully removed",r"(comp_task)","from the todo list")   
         
         case "exit":
@@ -65,4 +64,3 @@ while True:
         case _:
             print("you entered an unknown command ")
 print ("good luck working. ")
-#dir(list) to see functions. good luck exploring   
